4f5bd35de09d595b50cff6d45dc62f4cb98e829f
Iswenzz <suxlolz1528@gmail.com>
2020-10-29 11:48:53 +0100

    Moved entity tests & started template test for repositories/controllers

--- server/migrations/Version20201029085710.php
+++ server/migrations/Version20201029085710.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201028132408 extends AbstractMigration
+final class Version20201029085710 extends AbstractMigration
 {
     public function getDescription() : string
     {
