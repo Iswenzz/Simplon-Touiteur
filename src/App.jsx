1faec05887c9855c07c24eb7d62a2836fb8f903f
Iswenzz <suxlolz1528@gmail.com>
2020-10-29 16:16:29 +0100

    Responsive fonts & override material ui theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,94 @@ import React from "react";
 import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
 import {ThemeProvider, CssBaseline} from "@material-ui/core";
 import Home from "./containers/Home/Home";
+import "./Common.scss";
 
-const theme = responsiveFontSizes(createMuiTheme());
+const theme = responsiveFontSizes(createMuiTheme({
+	typography: {
+		subtitle1: {
+			fontFamily: "Roboto",
+			color: "rgba(220, 220, 220, 1)"
+		},
+		subtitle2: {
+			fontFamily: "Roboto",
+			color: "rgba(220, 220, 220, 1)"
+		},
+		h1: {
+			fontFamily: "Roboto",
+			color: "rgba(220, 220, 220, 1)"
+		},
+		h2: {
+			fontFamily: "Roboto",
+			color: "rgba(220, 220, 220, 1)"
+		},
+		h3: {
+			fontFamily: "Roboto",
+			color: "rgba(220, 220, 220, 1)"
+		},
+		h4: {
+			fontFamily: "Roboto",
+			color: "rgba(220, 220, 220, 1)"
+		},
+		h5: {
+			fontFamily: "Roboto",
+			color: "rgba(220, 220, 220, 1)"
+		},
+		caption: {
+			fontFamily: "Roboto",
+			color: "rgba(240, 240, 240, 1)"
+		}
+	},
+	overrides: {
+		MuiCssBaseline: {
+			"@global": {
+				html: {
+					"--scrollbarBG": "#23272a",
+					"--thumbBG": "#3a3d41",
+					overflowX: "hidden",
+					overflowY: "visible"
+				},
+				body: {
+					scrollbarWidth: "thin",
+					scrollbarColor: "var(--thumbBG) var(--scrollbarBG)",
+					backgroundColor: "#282C34",
+					margin: 0,
+				},
+				ul: {
+					listStyle: "none",
+					margin: 0,
+					padding: 0
+				},
+				"::-webkit-scrollbar": {
+					width: "12px"
+				},
+				"::-webkit-scrollbar-track": {
+					background: "var(--scrollbarBG)",
+					borderRadius: "10px"
+				},
+				"::-webkit-scrollbar-thumb": {
+					backgroundColor: "var(--thumbBG)",
+					border: "3px solid var(--scrollbarBG)",
+					borderRadius: "10px",
+				},
+			}
+		},
+		MuiTooltip: {
+			tooltip: {
+				fontSize: "1em",
+			}
+		},
+		MuiFab: {
+			primary: {
+				backgroundColor: "#282C34"
+			},
+		},
+		MuiDialogTitle: {
+			root: {
+				backgroundColor: "#282C34"
+			}
+		}
+	},
+}));
 
 export const App = () =>
 {
--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Home.scss";
+import {Typography} from "@material-ui/core";
 
 export const Home = () =>
 {
 	return (
-		<h1>Test</h1>
+		<Typography variant={"h3"} component={"h1"}>
+			Home Page
+		</Typography>
 	);
 };
 
