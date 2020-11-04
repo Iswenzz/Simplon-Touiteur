9763dc1dc2a48c3b7f441f99c0551ef0bf4c0845
Iswenzz <suxlolz1528@gmail.com>
2020-11-04 14:36:30 +0100

    EditProfile: sticky header + responsive modal

--- src/containers/Profile/EditProfile.jsx
+++ src/containers/Profile/EditProfile.jsx
@@ -1,31 +1,41 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import {
-	Container, Grid, Paper, TextField, Fab, Button,
+import {Grid, Paper, TextField, Fab, Button,
 	IconButton, Typography, Modal, Backdrop, Avatar, Box,
-}
-	from "@material-ui/core";
+} from "@material-ui/core";
 import { CameraAlt, Close } from "@material-ui/icons";
-
 
 const useStyles = makeStyles((theme) => ({
 	root: {
 		flexGrow: 1,
+		margin: "auto",
+		borderBottomLeftRadius: "1rem",
+		borderBottomRightRadius: "1rem",
 		height: "90%",
-		marginTop: "2.5rem",
-		borderRadius: "1rem",
-		font: "inherit",
-		marginLeft: "2.5rem",
 		[theme.breakpoints.down("md")]: {
-			margin: "2.5rem auto",
 			width: "100%"
 		},
 		[theme.breakpoints.up("lg")]: {
-			margin: "2.5rem auto",
 			width: "50%"
 		},
 		overflowY: "auto",
 		overflowX: "hidden",
+	},
+	header: {
+		marginTop: "2.5rem",
+		display: "flex",
+		margin: "auto",
+		justifyContent: "space-between",
+		flexDirection: "row",
+		backgroundColor: "rgb(40, 40, 40)",
+		borderTopLeftRadius: "1rem",
+		borderTopRightRadius: "1rem",
+		[theme.breakpoints.down("md")]: {
+			width: "100%"
+		},
+		[theme.breakpoints.up("lg")]: {
+			width: "50%"
+		},
 	},
 	form: {
 		width: "100%",
@@ -52,7 +62,6 @@ const useStyles = makeStyles((theme) => ({
 	darkArea: {
 		backgroundColor: "rgb(204, 214, 221)",
 		height: "12rem",
-		marginTop: "1rem",
 		opacity: "0.75",
 	},
 	icon: {
@@ -78,16 +87,11 @@ const useStyles = makeStyles((theme) => ({
 			backgroundColor: "rgba(29,161,242,1.00)",
 		},
 	},
-	header: {
-		display: "flex",
-		justifyContent: "space-between",
-		flexDirection: "row",
-	},
 	typo: {
 		font: "inherit",
 		fontSize: "25px",
-		marginTop: "1rem",
 		fontWeight: "bold",
+		alignSelf: "center"
 	},
 	textField: {
 		width: "90%",
@@ -102,143 +106,147 @@ const useStyles = makeStyles((theme) => ({
 
 const ModalContent = (props) => {
 	const classes = useStyles();
+
 	return (
-		<Grid container className={classes.root}>
-			<Paper className={classes.paper}>
-				<Grid component="nav" className={classes.header} item>
-					<div style={{ display: "flex", flexDirection: "row" }}>
-						<IconButton onClick={props.closeModal}>
-							<Close className={classes.icon} />{" "}
-						</IconButton>
-						<Typography className={classes.typo} variant="h4">
-                            Edit profile
-						</Typography>
+		<>
+			<Grid component="nav" className={classes.header} item>
+				<div style={{ display: "flex", flexDirection: "row" }}>
+					<IconButton onClick={props.closeModal}>
+						<Close className={classes.icon} />
+					</IconButton>
+					<Typography className={classes.typo} variant="h4">
+						Edit profile
+					</Typography>
+				</div>
+				<Fab
+					variant="extended"
+					size="small"
+					className={classes.btn}
+					type="submit"
+				>
+					<div className={classes.btnDiv}>
+						<span>Save</span>
 					</div>
-					<Fab
-						variant="extended"
-						size="small"
-						className={classes.btn}
-						type="submit"
-					>
-						<div className={classes.btnDiv}>
-							<span>Save</span>
-						</div>
-					</Fab>
-				</Grid>
-				<Grid className={classes.darkArea} item>
-					<input
-						accept="image/*"
-						className={classes.input}
-						id="contained-button-file"
-						multiple
-						type="file"
-					/>
-					<label htmlFor="contained-button-file">
-						<IconButton
-							className={classes.camera}
-							aria-label="upload picture"
-							component="span"
-						>
-							<CameraAlt className={classes.icon} />
-						</IconButton>
-					</label>
-					<div className={classes.avatarBox}>
-						<Box>
-							<Avatar className={classes.avatar}>
-								<input
-									accept="image/*"
-									className={classes.input}
-									id="contained-button-file"
-									multiple
-									type="file"
-								/>
-								<label htmlFor="contained-button-file">
-									<IconButton aria-label="upload picture" component="span">
-										<CameraAlt className={classes.icon} />
-									</IconButton>
-								</label>
-							</Avatar>
-						</Box>
-					</div>
-				</Grid>
-				<form className={classes.form} noValidate>
-					<Grid container spacing={2}>
-						<Grid item xs={12}>
-							<TextField
-								className={classes.textField}
-								name="Name"
-								variant="filled"
-								fullWidth
-								id="Name"
-								label="Name"
-								autoFocus
-							/>
-						</Grid>
-						<Grid item xs={12}>
-							<TextField
-								className={classes.textField}
-								name="Bio"
-								variant="filled"
-								multiline
-								fullWidth
-								id="Bio"
-								label="Bio"
-								autoFocus
-							/>
-						</Grid>
-						<Grid item xs={12}>
-							<TextField
-								className={classes.textField}
-								name="location"
-								variant="filled"
-								fullWidth
-								placeholder="Add your location"
-								id="location"
-								label="Location"
-								autoFocus
-							/>
-						</Grid>
-						<Grid item xs={12}>
-							<TextField
-								className={classes.textField}
-								name="website"
-								variant="filled"
-								required
-								placeholder="Add your website"
-								fullWidth
-								id="website"
-								label="website"
-								autoFocus
-							/>
-						</Grid>
-					</Grid>
-				</form>
-				<Grid item xs={12}>
-					<div style={{ marginLeft: "5%" }}>
-						<Typography>
-                            Birth date.{" "}
-							<Button
-								style={{
-									color: "rgba(29,161,242,1.00)",
-									textTransform: "capitalize",
-								}}
+				</Fab>
+			</Grid>
+			<Grid container className={classes.root}>
+				<Paper className={classes.paper}>
+					<Grid className={classes.darkArea} item>
+						<input
+							accept="image/*"
+							className={classes.input}
+							id="contained-button-file"
+							multiple
+							type="file"
+						/>
+						<label htmlFor="contained-button-file">
+							<IconButton
+								className={classes.camera}
+								aria-label="upload picture"
+								component="span"
 							>
-                                Edit
-							</Button>
-						</Typography>
-					</div>
-					<div style={{ marginLeft: "5%" }}>
-						<Typography>Add your date of birth</Typography>
-					</div>
-				</Grid>
-			</Paper>
-		</Grid>
+								<CameraAlt className={classes.icon} />
+							</IconButton>
+						</label>
+						<div className={classes.avatarBox}>
+							<Box>
+								<Avatar className={classes.avatar}>
+									<input
+										accept="image/*"
+										className={classes.input}
+										id="contained-button-file"
+										multiple
+										type="file"
+									/>
+									<label htmlFor="contained-button-file">
+										<IconButton aria-label="upload picture" component="span">
+											<CameraAlt className={classes.icon} />
+										</IconButton>
+									</label>
+								</Avatar>
+							</Box>
+						</div>
+					</Grid>
+					<form className={classes.form} noValidate>
+						<Grid container spacing={2}>
+							<Grid item xs={12}>
+								<TextField
+									className={classes.textField}
+									name="Name"
+									variant="filled"
+									fullWidth
+									id="Name"
+									label="Name"
+									autoFocus
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									className={classes.textField}
+									name="Bio"
+									variant="filled"
+									multiline
+									fullWidth
+									id="Bio"
+									label="Bio"
+									autoFocus
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									className={classes.textField}
+									name="location"
+									variant="filled"
+									fullWidth
+									placeholder="Add your location"
+									id="location"
+									label="Location"
+									autoFocus
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									className={classes.textField}
+									name="website"
+									variant="filled"
+									required
+									placeholder="Add your website"
+									fullWidth
+									id="website"
+									label="website"
+									autoFocus
+								/>
+							</Grid>
+						</Grid>
+					</form>
+					<Grid item xs={12}>
+						<div style={{ marginLeft: "5%" }}>
+							<Typography>
+								Birth date.{" "}
+								<Button
+									style={{
+										color: "rgba(29,161,242,1.00)",
+										textTransform: "capitalize",
+									}}
+								>
+									Edit
+								</Button>
+							</Typography>
+						</div>
+						<div style={{ marginLeft: "5%" }}>
+							<Typography>Add your date of birth</Typography>
+						</div>
+					</Grid>
+				</Paper>
+			</Grid>
+		</>
 	);
 };
 
 const EditProfile = (props) => {
 	const classes = useStyles();
 	const ref = React.createRef();
+
 	return (
 		<Modal
 			ref={ref}
