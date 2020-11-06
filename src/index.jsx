2289bd258e887e864b97ad7c267e5ecb871f96bb
Iswenzz <suxlolz1528@gmail.com>
2020-11-06 11:06:32 +0100

    Auth: cache axios auth headers

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import axios from "axios";
+
+// auth cache
+axios.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem("auth") ?? "fail"}`;
 
 ReactDOM.render(<App />, document.querySelector("#app"));
